33f3e06da7b8801c9779ca49c07a2941a65fc81a
Ghazy <gazi566456@gmail.com>
2026-04-01 00:46:26 +0300

    Add HomeScreen with image grid display
    Implement HomeScreen to display images in a grid layout.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import '../services/storage_service.dart';
 
+class HomeScreen extends StatefulWidget {
+  final String path;
+  HomeScreen({required this.path});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<File> files = [];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  void load() async {
+    files = await loadStatuses(widget.path);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("الحالات")),
+      body: GridView.builder(
+        itemCount: files.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, index) {
+          final file = files[index];
+          return Image.file(file, fit: BoxFit.cover);
+        },
+      ),
+    );
+  }
+}
