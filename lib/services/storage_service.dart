98d3cc8e10143b104825e66ead28cf10142816cc
Ghazy <gazi566456@gmail.com>
2026-04-01 00:45:38 +0300

    Add loadStatuses function to filter media files

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,1 +1,11 @@
+import 'dart:io';
 
+Future<List<File>> loadStatuses(String path) async {
+  final dir = Directory(path);
+
+  return dir.listSync().whereType<File>().where((file) {
+    return file.path.endsWith(".jpg") ||
+           file.path.endsWith(".png") ||
+           file.path.endsWith(".mp4");
+  }).toList();
+}
